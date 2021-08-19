8ceaa2c5c621c67d54b44cd18e77bdcc72926ddb
Kevin Long <kevin.uu.long@gmail.com>
2021-08-19 15:14:41 -0400

    Remove unused code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -74,23 +74,12 @@ async function loadSwim(swim) {
         .then(res => res.json())
         .then(res => {
             // Set the title
-            // document.getElementById("title").innerText = res.title;
-            // document.title = res.title;
             setTitle(res.title);
 
             // Set the date
-            // let date = res.date;
-            // date = date.replace(/-/g, '/');
-            // document.getElementById("date").innerText = date;
             setDate(res.date);
 
             // Tags
-            // res.tags.forEach(tag => {
-            //     let elem = document.createElement('p');
-            //     elem.className = "tag";
-            //     elem.textContent = tag;
-            //     document.getElementById("meta").appendChild(elem);
-            // });
             setTags(res.tags);
 
             // Set sections
@@ -98,13 +87,6 @@ async function loadSwim(swim) {
             res.body.forEach(section => {
                 let sectionTitle = Object.keys(section)[0];
                 createSection(sectionTitle);
-                // let title = document.createElement('h2');
-                // title.textContent = sectionTitle;
-                // document.getElementById("body").appendChild(title);
-
-                // let distance = document.createElement('p');
-                // distance.className = "distance";
-                // document.getElementById("body").appendChild(distance);
 
                 let sectionTotal = 0;
 
@@ -114,28 +96,6 @@ async function loadSwim(swim) {
                     data.i = i;
                     sectionTotal += createExercise(data);
                     i++;
-                    // let exercise = document.createElement('div');
-                    // exercise.className = "exercise";
-
-                    // let number = document.createElement('p');
-                    // number.className = "number";
-                    // number.innerText = `${i}.`;
-                    // exercise.appendChild(number);
-                    // i++;
-
-                    // let details = document.createElement('p');
-                    // details.className = "details";
-                    // details.innerText = `${data.repetitions} x ${data.distance} ${data.stroke}`;
-                    // exercise.appendChild(details);
-
-                    // sectionTotal += data.repetitions * data.distance;
-
-                    // let description = document.createElement('p');
-                    // description.className = "description";
-                    // description.innerText = data.cycle ? `@${data.cycle} ${data.description}` : data.description;
-                    // exercise.appendChild(description);
-
-                    // document.getElementById("body").appendChild(exercise);
                 })
 
                 let distances = document.querySelectorAll(".distance:not(#total)")
